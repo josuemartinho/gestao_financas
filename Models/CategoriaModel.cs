94c7d2f0bfee7b6f1a26bae9b9f13efe6f86adaa
josuemartinho <josuem4rtinho@gmail.com>
2023-11-01 13:53:58 +0000

    editando categoria

--- Models/CategoriaModel.cs
+++ Models/CategoriaModel.cs
@@ -7,7 +7,7 @@ namespace sistemaFinanceiro.Models
     public class CategoriaModel
     {
         [Key]
-        public int CategoriaId {  get; set; }
+        public int CategoriaId { get; set; }
 
         [Column(TypeName = "nvarchar(50)")]
         public string Nome { get; set; }
@@ -15,5 +15,13 @@ namespace sistemaFinanceiro.Models
         public string Icone { get; set; } = "";
         [Column(TypeName = "nvarchar(10)")]
         public string Tipo { get; set; } = "Despesa";
+        [NotMapped]
+        public string? IconeComNome
+        {
+            get
+            {
+                return this.Icone + " " + this.Nome;
+            }
+        }
     }
 }
